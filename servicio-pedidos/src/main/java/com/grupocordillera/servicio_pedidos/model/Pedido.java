facfc0c2194f864b445d2780541d37784733d330
dvmsndvl <dami.sandoval@duocuc.cl>
2026-05-13 20:43:31 -0400

    Update Pedido.java

--- servicio-pedidos/src/main/java/com/grupocordillera/servicio_pedidos/model/Pedido.java
+++ servicio-pedidos/src/main/java/com/grupocordillera/servicio_pedidos/model/Pedido.java
@@ -1,7 +1,6 @@
 package com.grupocordillera.servicio_pedidos.model;
 
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,19 +11,19 @@ import lombok.Data;
 
 @Entity
 @Table(name = "pedidos")
-@Data // Genera getters, setters y constructores con Lombok
+@Data
 public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String sucursalOrigen; // Ciudad o sucursal [cite: 5]
+    private String sucursalOrigen;
     private Double montoTotal;
-    private String estado; // Pendiente, Completado, Cancelado
+    private String estado;
     private LocalDateTime fechaCreacion;
 
     @PrePersist
     protected void onCreate() {
-        fechaCreacion = LocalDateTime.now();
+        this.fechaCreacion = LocalDateTime.now();
     }
 }
