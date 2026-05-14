fa45858d2beab0170b62ae36e0e4a76e5bc1d535
dvmsndvl <dami.sandoval@duocuc.cl>
2026-05-13 20:42:44 -0400

    Update PedidoController.java

--- servicio-pedidos/src/main/java/com/grupocordillera/servicio_pedidos/controller/PedidoController.java
+++ servicio-pedidos/src/main/java/com/grupocordillera/servicio_pedidos/controller/PedidoController.java
@@ -1,9 +1,8 @@
 package com.grupocordillera.servicio_pedidos.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity; // Importación recomendada
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +20,6 @@ public class PedidoController {
     @Autowired
     private PedidoRepository repository;
 
-
     @GetMapping("/resumen-ventas")
     public ResponseEntity<List<Pedido>> obtenerVentasConsolidadas() {
         List<Pedido> pedidos = repository.findAll();
@@ -33,7 +31,6 @@ public class PedidoController {
         return repository.findAll();
     }
 
-
     @GetMapping("/sucursal/{nombre}")
     public List<Pedido> obtenerPorSucursal(@PathVariable String nombre) {
         return repository.findBySucursalOrigen(nombre);
@@ -41,7 +38,6 @@ public class PedidoController {
 
     @PostMapping("/crear")
     public Pedido crearPedido(@RequestBody Pedido nuevoPedido) {
-
         return repository.save(nuevoPedido);
     }
 }
